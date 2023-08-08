d6c42ca614de103551de54079e9581e95a360b9b
nuno <nuno@ion.lu>
2023-08-08 11:16:10 +0100

    init commit

---
+++ src/App.vue
@@ -0,0 +1,98 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import YouTubePlayerPlus from 'youtube-player-plus'
+
+const url = ref()
+const player = ref()
+const videosIds = ref(['wJZm7WPctK8', 'pZ5NxMN88Jg'])
+const videos = ref([])
+
+onMounted(() => {
+  // create localStorage
+  const Db = localStorage.getItem('vids')
+  if ( ! Db ){
+    localStorage.setItem('vids', videosIds.value)
+  }
+
+  buildList()
+
+  const options = {
+    autoplay: 1,
+    controls: false,
+    annotations: false,
+    modestBranding: true,
+    relatedVideos: false,
+  }
+  player.value = new YouTubePlayerPlus('#player', options)
+  player.value.load('wJZm7WPctK8', true)
+})
+
+async function add(){
+  console.log(url.value)
+  const videoId = url.value.split("v=")[1]
+  // .substring(0, 11)
+  console.log(videoId)
+  url.value = ''
+  videosIds.value.push(videoId)
+  await localStorage.setItem('vids', videosIds.value)
+  buildList()
+}
+
+function update(videoId){
+  console.log(videoId)
+  player.value.load(videoId, true)
+}
+
+async function buildList(){
+  videos.value = []
+  const vids = await localStorage.getItem('vids').split(',')
+  vids.forEach(video => {
+    const thumbnail = `https://img.youtube.com/vi/${video}/mqdefault.jpg`
+    videos.value.push({ id: video, thumbnail })
+  })
+
+}
+</script>
+
+<template>
+  <div>
+      <form @submit.prevent="add" class="url">
+        <input type="text" id="url" v-model="url">
+        <button>Guardar</button>
+      </form>
+      <div id="player">
+      </div>
+      <div class="list">
+        <div v-for="(video, idx) in videos" :key="idx">
+          <img :src="video.thumbnail" @click="update(video.id)">
+        </div>
+      </div>
+  </div>
+</template>
+
+<style scoped>
+.url {
+  display: flex;
+  justify-content: space-between;
+  margin: 0 0 20px 0;
+  padding: 10px;
+}
+.url input {
+  flex: 1;
+  margin-right: 20px;
+}
+.list {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  gap: 20px;
+  margin-top: 40px;
+}
+
+.list img {
+  border: 1px solid;
+}
+
+.list img {
+  cursor: pointer;
+}
+</style>
